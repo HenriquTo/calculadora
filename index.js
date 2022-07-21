4b70d5294641ed62f3d56b4b7843f187672ee875
Henrique Alves de Lima <henriquetolis@outlook.com>
2022-07-21 19:55:47 -0300

    Atualizacao
    Atualizado o index.html
    
    criado as paginas README.md | index.js |  estilo.css

---
+++ index.js
@@ -0,0 +1,16 @@
+var n1 = prompt('Digite um número');
+n1 = parseFloat(n1);
+
+function consulta() {
+    var n = prompt('Digite um número');
+    n = parseFloat(n);
+    escreva.innerHTML = n1;
+}
+   
+var escreva = document.querySelector('#display');
+   
+escreva.innerHTML = n1;
+
+var texto = '';
+  
+   
