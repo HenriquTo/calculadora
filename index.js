ababfd3931f19aaaa19ab5a2da5f5638f433eb13
Henrique Alves de Lima <henriquetolis@outlook.com>
2022-07-21 23:02:27 -0300

    atualização 2.0
    Feito mudanças na pagina js

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-var n1 = prompt('Digite um número');
-n1 = parseFloat(n1);
-
-function consulta() {
-    var n = prompt('Digite um número');
-    n = parseFloat(n);
+function um() {
+    var n1= 1
     escreva.innerHTML = n1;
+}
+function dois() {
+    var n1= 2
+    escreva.innerHTML = n1;
+}
+function tres() {
+    var n1= 3
+    escreva.innerHTML = n1;
+}
+function quatro() {
+    var n1= 4
+    escreva.innerHTML = n1;
+}
+function cinco() {
+    var n1= 5
+    escreva.innerHTML = n1;
+}
+function seis() {
+    var n1= 6
+    escreva.innerHTML = n1;
+}
+function sete() {
+    var n1= 7
+    escreva.innerHTML = n1;
+}
+function oito() {
+    var n1= 8
+    escreva.innerHTML = n1;
+}
+function nove() {
+    var n1= 9
+    escreva.innerHTML = n1;
+}
+function zero() {
+    var n1= 0
+    escreva.innerHTML = n1;
+}
+function ponto() {
+    var n1= ','
+    escreva.innerHTML = n1;
+}
+function mais() {
+    var soma= n1+n1
+    escreva.innerHTML = soma;
 }
    
 var escreva = document.querySelector('#display');
    
-escreva.innerHTML = n1;
+escreva.innerHTML = texto;
 
-var texto = '';
+var limpa = '';
   
    
